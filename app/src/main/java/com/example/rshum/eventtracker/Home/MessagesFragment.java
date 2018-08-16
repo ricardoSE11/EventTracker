c3aac03963dbb718aec5e815e64c9237b7b896f7
rshum <ricardoshumestrada@gmail.com>
2018-08-16 02:48:42 -0600

    [new] Resource files (drawable)

---
+++ app/src/main/java/com/example/rshum/eventtracker/Home/MessagesFragment.java
@@ -0,0 +1,23 @@
+package com.example.rshum.eventtracker.Home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rshum.eventtracker.R;
+
+
+public class MessagesFragment extends Fragment {
+    private static final String TAG = "MessagesFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_messages, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/rshum/eventtracker/Profile/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.example.rshum.eventtracker.Profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rshum.eventtracker.R;
+
+public class ProfileFragment extends Fragment {
+
+    private static final String TAG = "ProfileFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        return view;
+    }
+}
